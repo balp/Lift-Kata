da7cc4d746286f803b17ece64775b9585767c9ac
Anders Arnholm <anders.arnholm@hiq.se>
2019-12-12 20:28:10 +0100

    Code from doing the first kata.

--- python/lift.py
+++ python/lift.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from typing import List
 from attr import dataclass
 
 
@@ -22,18 +22,29 @@ class Lift:
         self.doors_open = doors_open
         self.requested_floors = list(requested_floors) if requested_floors else []
 
+    def request(self, floor: int) -> None:
+        self.requested_floors.append(floor)
+
+    def tick(self):
+        if self.floor == self.requested_floors[0]:
+            self.doors_open = True
+            self.requested_floors.pop(0)
+        elif self.doors_open:
+            self.doors_open = False
+        else:
+            self.floor = self.requested_floors[0]
 
 class LiftSystem:
     def __init__(self, floors=None, calls=None, lifts=None):
         self.floors = list(floors) if floors else []
         self.calls = list(calls) if calls else []
-        self.lifts = list(lifts) if lifts else []
+        self.lifts: List[Lift] = list(lifts) if lifts else []
 
     def calls_for(self, floor):
         return [c for c in self.calls if c.floor == floor]
 
     def tick(self):
-        # TODO: implement this method
-        pass
+        for lift in self.lifts:
+            lift.tick()
 
 
--- python/test/test_lift.py
+++ python/test/test_lift.py
@@ -4,9 +4,41 @@ from lift import Lift, LiftSystem
 from lift_printers import print_lifts
 
 
-# TODO: finish writing this test (remove the 'todo' prefix so the test will get run)
-def todo_test_something():
-    liftA = Lift("A", 0)
-    lifts = LiftSystem(floors=[0,1], lifts=[liftA])
+def test_request_to_move():
+    lifts, lifts_before_tick = set_up_simple_lift_system(start_floor=2, request_floor=1, doors_open=False)
     lifts.tick()
-    verify(print_lifts(lifts))
+    lifts_after_tick = print_lifts(lifts)
+    _verify_lifts(lifts_after_tick, lifts_before_tick)
+
+
+def test_on_requested_floor_open_door():
+    lifts, lifts_before_tick = set_up_simple_lift_system(start_floor=1, request_floor=1, doors_open=False)
+    lifts.tick()
+    lifts_after_tick = print_lifts(lifts)
+    _verify_lifts(lifts_after_tick, lifts_before_tick)
+
+
+def test_dont_move_on_request_with_open_doors():
+    lifts, lifts_before_tick = set_up_simple_lift_system(start_floor=1, request_floor=2, doors_open=True)
+    lifts.tick()
+    lifts_after_tick = print_lifts(lifts)
+    _verify_lifts(lifts_after_tick, lifts_before_tick)
+
+
+def set_up_simple_lift_system(start_floor, request_floor, doors_open):
+    lift_a = Lift("A", start_floor)
+    lift_a.request(request_floor)
+    lift_a.doors_open = doors_open
+    lifts = LiftSystem(floors=[1, 2], lifts=[lift_a])
+    lifts_before_tick = print_lifts(lifts)
+    return lifts, lifts_before_tick
+
+
+def _verify_lifts(lifts_after_tick, lifts_before_tick):
+    verify(f"""Before:
+{lifts_before_tick}
+
+After:
+
+{lifts_after_tick}""")
+
